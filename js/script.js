38f7f3e9fa3b779164f9a3f56abb0fe5bd80e93c
mattialicausi <licausivitomattia@gmail.com>
2022-11-02 21:13:54 +0100

    modifiche in main-right-side per aggiungere messaggi, da sistemare

--- js/script.js
+++ js/script.js
@@ -13,14 +13,14 @@
                 status: 'sent'
             }, 
             {
-            date: '10/01/2020 15:50:00',
-            message: 'Ricordati di stendere i panni',
-            status: 'sent'
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
             },
             {
-            date: '10/01/2020 16:15:22',
-            message: 'Tutto fatto!',
-            status: 'received'
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
             } 
         ],
     }, 
@@ -37,15 +37,14 @@
                 status: 'sent'
             }, 
             {
-
-            date: '20/03/2020 16:30:55',
-            message: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+                date: '20/03/2020 16:30:55',
+                message: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
             },
             {
-            date: '20/03/2020 16:35:00',
-            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+                date: '20/03/2020 16:35:00',
+                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
             }
         ],
     },
